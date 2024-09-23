0fa1a7404cf4227596411a9e686cf21cbd2eb171
MinyazevR <eeuriset8@gmail.com>
2024-09-23 23:15:54 +0300

    Remove mHelper

--- tests/trikNetworkTests/trikNetworkTests.h
+++ tests/trikNetworkTests/trikNetworkTests.h
@@ -35,9 +35,6 @@ protected:
 	trikNetwork::MailboxInterface *prepareHost(int port, int hullNumber, int portToConnect);
 	void cleanUp();
 private:
-	/// Does nothing, but ensures event processing at the time of destruction of test suite, to avoid
-	/// deleteLater()-related memleaks.
-	trikKernel::DeinitializationHelper mHelper;
 	QList<QThread *> mWorkers;
 	QScopedPointer<trikControl::BrickInterface> mBrick;
 	QScopedPointer<trikNetwork::MailboxInterface> mMailboxInterface;
